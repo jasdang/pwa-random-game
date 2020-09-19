0474499e6ce21fe155f373b0f12f85067f99f79d
Jasmine Dang <ngandth.1992@gmail.com>
2020-09-19 22:27:52 +0800

    revert to 8971f5e

--- src/App.js
+++ src/App.js
@@ -6,19 +6,54 @@ const AppContainer = styled.div`
   width: 100vw;
   margin: 0;
   background-color: blue;
+  @media screen and (min-width: 376px) {
+    width: 376px;
+  }
+`;
+
+const Header = styled.div`
+  font-size: 2rem;
+  width: fit-content;
+  margin: 0 auto;
+  padding: 1rem 0;
 `;
 
 const Box = styled.div`
   background: green;
-  height: 30vh;
-  text-align: center;
+  width: 80%;
+  margin: 0 auto;
+  margin: 1rem auto;
+  font-size: 8rem;
+  position: relative;
+  &:after {
+    padding-bottom: 100%;
+    content: '';
+    display: block;
+  }
+`;
+
+const BoxContent = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Flex = styled.div`
   display: flex;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: center;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  display: block;
+  width: 80%;
+  height: 3rem;
+  margin: 1rem auto;
+`;
 
 function App() {
   return (
