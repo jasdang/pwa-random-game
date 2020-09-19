f1fc8afb45386fdfafee983b41e484d98e2bbd94
Jasmine Dang <ngandth.1992@gmail.com>
2020-09-19 13:15:33 +0800

    guessing-basic html skeleton

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ const AppContainer = styled.div`
   height: 100vh;
   width: 100vw;
   margin: 0;
-  ${'' /* background-color: blue; */}
+  background-color: blue;
 `;
 
+const Box = styled.div`
+  background: green;
+  height: 30vh;
+  text-align: center;
+`;
+
+const Flex = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button``;
+
 function App() {
-  return <AppContainer>hello</AppContainer>;
+  return (
+    <AppContainer>
+      <div>Random Game</div>
+      <div>Guessing Game</div>
+      <Box>?</Box>
+      <Flex>
+        <p>Your guess </p>
+        <input type='number' min='0' max='100' />
+      </Flex>
+      <Button>Submit</Button>
+    </AppContainer>
+  );
 }
 
 export default App;
