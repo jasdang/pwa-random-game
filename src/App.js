586b0b4aaee4eae3ec52465c63f2e2af5c3144ab
Jasmine Dang <ngandth.1992@gmail.com>
2020-09-19 17:04:34 +0800

    display phone size across screen size

--- src/App.js
+++ src/App.js
@@ -4,28 +4,65 @@ import styled from 'styled-components';
 const AppContainer = styled.div`
   height: 100vh;
   width: 100vw;
-  margin: 0;
+  margin: 0 auto;
   background-color: blue;
+  @media screen and (min-width: 376px) {
+    width: 376px;
+  }
+`;
+
+const Header = styled.div`
+  font-size: 2rem;
+  width: fit-content;
+  margin: 0 auto;
+  padding: 1rem 0;
 `;
 
 const Box = styled.div`
   background: green;
-  height: 30vh;
-  text-align: center;
+  width: 80%;
+  margin: 0 auto;
+  margin: 1rem auto;
+  font-size: 8rem;
+  position: relative;
+  &:after {
+    padding-bottom: 100%;
+    content: '';
+    display: block;
+  }
+`;
+
+const BoxContent = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Flex = styled.div`
   display: flex;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: center;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  display: block;
+  width: 80%;
+  height: 3rem;
+  margin: 1rem auto;
+`;
 
 function App() {
   return (
     <AppContainer>
-      <div>Random Game</div>
-      <div>Guessing Game</div>
-      <Box>?</Box>
+      <Header>Random Game</Header>
+      <Header>Guessing Game</Header>
+      <Box>
+        <BoxContent>?</BoxContent>
+      </Box>
       <Flex>
         <p>Your guess </p>
         <input type='number' min='0' max='100' />
