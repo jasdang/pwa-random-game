4a9990208875766272e303cc0dedd67a4d8847f9
Jasmine Dang <ngandth.1992@gmail.com>
2020-09-23 22:09:31 +0800

    clean up

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import {createActions, handleActions, combineActions} from 'redux-actions';
+import {handleActions} from 'redux-actions';
 import {updateMin, updateMax} from './actions';
 
 export const initialStore = {
@@ -9,6 +9,7 @@ export const initialStore = {
     max: 100,
   },
 };
+
 const guessingGame = handleActions(
   {
     [updateMin]: (state, {payload}) => {
@@ -27,16 +28,8 @@ const guessingGame = handleActions(
   initialStore
 );
 
-// const guessingGame = (state = [], action) => {
-//   const {type, payload} = action;
-//   switch (type) {
-//     default:
-//       return state;
-//   }
-// };
-
 const nextGame = (state = [], action) => {
-  const {type, payload} = action;
+  const {type} = action;
   switch (type) {
     default:
       return state;
